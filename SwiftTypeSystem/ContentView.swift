f01b2aef3e95d46442fa31698474d8514a021aff
oscarmgit <molinameta18@gmail.com>
2023-03-16 12:56:53 -0400

    Initial stab at representing the Swift type system

---
+++ SwiftTypeSystem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftTypeSystem
+//
+//  Created by Peniel Ansa on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("SwiftTypeSystem")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTypeSystem/SwiftTypeSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTypeSystemApp.swift
+//  SwiftTypeSystem
+//
+//  Created by Peniel Ansa on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTypeSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
